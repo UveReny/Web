62218c09d17e54badf06385e2a88f9eea5b39a58
user <kireevnv@sibsau.ru>
2024-09-16 16:10:40 +0700

    Add WebView

--- app/src/main/java/com/example/web/MainActivity.kt
+++ app/src/main/java/com/example/web/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.web
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView : WebView = findViewById(R.id.webView)
+        webView.loadUrl("https://www.example.com")
     }
 }
